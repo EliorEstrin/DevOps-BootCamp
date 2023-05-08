39977b7f2e59546470ab3d071049252a050bdbe9
elior <elior1001@gmail.com>
2023-05-08 11:55:53 +0300

    Added Gutendex Repo

---
+++ Gutendex/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Deleting the content of the static file as proof the nginx serving the front end
+rm -rf /app/static/*
+
+# Set up the database for storing the catalog data:
+python manage.py migrate
+
+# Populate the Database
+python manage.py updatecatalog
+
+
+python manage.py runserver "0.0.0.0:8000"
